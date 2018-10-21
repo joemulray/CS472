ae9645bd4111cf2e7c0b80d397abeede302a6eea
Joseph Mulray <joe.mulray@gmail.com>
2018-10-21 14:30:51 -0400

    added usage to logging

--- HW2/logging.py
+++ HW2/logging.py
@@ -4,11 +4,29 @@
 Logger class for client and server
 """
 
+import os
 import datetime
 
 class logger:
+	"""
+	Logger encapsulates a filename, service, and time format
+	"""
 	def __init__(self, filename, service=None, format="%Y-%m-%d %H:%M"):
-		self.file = open(filename, 'w+')
+		"""
+		Constructs a new 'Logger' object
+
+		:param filename: name of file being logged to
+		:param service: name of service, eg. {client, server, app}
+		:param format: the format of the timestamp
+		:return: returns nothing
+		"""
+
+		#if file does not exist
+		if not os.path.exists(filename):
+			mode = "w+"
+		else:
+			mode = "w+"
+		self.file = open(filename, 'a')
 		self.service = service
 		self.format = format
 		self.debugmsg = "[debug]"
@@ -16,21 +34,69 @@ class logger:
 		self.timestamp = self.time()
 
 	def debug(self,msg):
-		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.debugmsg, str(msg))
+		"""
+		Display/write debug messages to file or stdout
+
+		:param msg: The message to be logged
+		:return: returns nothing
+		"""
+		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.debugmsg, str(msg).rstrip())
 		print log
 		self.file.write(log + "\n")
+		self.file.flush()
 
 	def error(self,msg):
-		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.errormsg, str(msg))
+		"""
+		Display/write errors messages to file or stdout
+
+		:param msg: The message to be logged
+		:return: returns nothing
+		"""
+		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.errormsg, str(msg).rstrip())
 		print log
 		self.file.write(log + "\n")
+		self.file.flush()
+
+
+	def usage(self, msg):
+		log =  "Usage: %s" %msg
+		print log
+		self.file.write(log + "\n")
+		self.file.flush()
+
 
 	def time(self):
+		"""
+		Contructs a timestamp
+
+		:return timestamp: returns string object of dateformat
+		"""
 		timenow = datetime.datetime.now()
 		return timenow.strftime(self.format)
 
 	def sending(self, value):
+		"""
+		Constructs Sent message for debug
+
+		:param value: value being sent to server
+		:return: returns nothing
+		"""
 		self.debug("Sent: " + str(value))
 
 	def received(self, value):
+		"""
+		Constructs Recieved message for debug
+
+		:param value: value being recieved from server
+		:return: returns nothing
+		"""
 		self.debug("Received: " + str(value))
+
+
+	def close(self):
+		"""
+		Closes the logger file
+
+		:return: returns nothing
+		"""
+		self.file.close()
