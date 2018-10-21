acd8180accc1b0fe1d7b2b0595e4d6e6b492801b
Joseph Mulray <joe.mulray@gmail.com>
2018-10-21 14:30:26 -0400

    working stor, pasv

--- HW2/client.py
+++ HW2/client.py
@@ -4,7 +4,7 @@ import socket
 import struct
 import sys
 import datetime
-
+from logging import logger
 """
 Joseph Mulray
 CS472 - Computer Networks
@@ -27,87 +27,7 @@ Homework Assignment #2
 
 
 
-BUFFER  = 77777
-
-class logger:
-	"""
-	Logger encapsulates a filename, service, and time format
-	"""
-	def __init__(self, filename, service=None, format="%Y-%m-%d %H:%M"):
-		"""
-		Constructs a new 'Logger' object
-
-		:param filename: name of file being logged to
-		:param service: name of service, eg. {client, server, app}
-		:param format: the format of the timestamp
-		:return: returns nothing
-		"""
-
-		self.file = open(filename, 'w+')
-		self.service = service
-		self.format = format
-		self.debugmsg = "[debug]"
-		self.errormsg = "[ERROR]"
-		self.timestamp = self.time()
-
-	def debug(self,msg):
-		"""
-		Display/write debug messages to file or stdout
-
-		:param msg: The message to be logged
-		:return: returns nothing
-		"""
-		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.debugmsg, str(msg).rstrip())
-		print log
-		self.file.write(log + "\n")
-
-	def error(self,msg):
-		"""
-		Display/write errors messages to file or stdout
-
-		:param msg: The message to be logged
-		:return: returns nothing
-		"""
-		log = "%s : %s %s : %s" %(self.timestamp, self.service, self.errormsg, str(msg).rstrip())
-		print log
-		self.file.write(log + "\n")
-
-	def time(self):
-		"""
-		Contructs a timestamp
-
-		:return timestamp: returns string object of dateformat
-		"""
-		timenow = datetime.datetime.now()
-		return timenow.strftime(self.format)
-
-	def sending(self, value):
-		"""
-		Constructs Sent message for debug
-
-		:param value: value being sent to server
-		:return: returns nothing
-		"""
-		self.debug("Sent: " + str(value))
-
-	def received(self, value):
-		"""
-		Constructs Recieved message for debug
-
-		:param value: value being recieved from server
-		:return: returns nothing
-		"""
-		self.debug("Received: " + str(value))
-
-	
-	def close(self):
-		"""
-		Closes the logger file
-
-		:return: returns nothing
-		"""
-		self.file.close()
-
+BUFFER  = 4024
 
 class FTP:
 	def __init__(self, socket=None, username="root", password="password"):
@@ -116,28 +36,40 @@ class FTP:
 		self.username = username
 		self.password = password
 		self.__authorized = False
+		self.datasocket = None
+		self.port = 20
+		self.action = { "login" : self.username, "port" : self.buildport, "eprt" : self.buildeprt, \
+					"retr" : self.buildretr, "stor" : self.buildstor }
 		self.commands = {
-						"user" : "USER", "pasv" : "PASV", "quit" : "QUIT", \
-						"pasv" : "PASV", "epsv" : "EPSV", "port" : "PORT", \
-						"eprt" : "EPRT", "retry" : "RETR", "stor" : "STOR", \
-						"syst" : "SYST", "list" : "LIST", "pwd" : "PWD", \
-						"help" : "HELP", "pass" : "PASS"
-						}
+					"user" : "USER", "quit" : "QUIT", "pasv" : "PASV", \
+					"epsv" : "EPSV", "port" : "PORT",  "cd" : "CWD", \
+					"eprt" : "EPRT", "retr" : "RETR", "stor" : "STOR", \
+					"syst" : "SYST", "list" : "LIST", "pwd" : "PWD", \
+					"help" : "HELP", "pass" : "PASS", "ls" : "DIR" \
+					}
 
 
 	def doProtocol(self):
 		while True:
 			response = self.socket.receive()
-			self.evaulate(response[:3])()
+			(function, response) = self.evaulate(response[:3], response)
+			function(response)
 
 
-
-	def command(self):
+	def command(self, response=None):
 		cmd = raw_input("ftp> ").split()
 		if cmd:
 			if cmd[0] in self.commands:
-				command = self.buildcmd(cmd)
-				self.socket.send(command)
+
+				if cmd[0] in self.action:
+					command = self.action[cmd[0]](cmd)
+				else:
+					command = self.buildcmd(cmd)
+
+				if command:
+					self.socket.send(command)
+				else:
+					self.command()
 
 			else:
 				self.command()
@@ -145,20 +77,44 @@ class FTP:
 			self.command()
 
 
-	def promptusername(self):
+	def promptusername(self, response=None):
 		user = "USER " + raw_input("ftp> Name: ") + "\r\n"
 		self.username = user
 		self.socket.send(self.username)
 
 
-	def promptpassword(self):
+	def promptpassword(self, response=None):
 		password = "PASS " + raw_input("ftp> Password: ") + "\r\n"
 		self.password = password
 		self.socket.send(self.password)
 
 
+	def openconnection(self, response):
+		print "\n** OPEN CONNECTION **\n"
+		if self.socket.datasocket:
+			respc = self.socket.datasocket.receive()
+		else:
+			self.datasocket = ClientSocket(sys.argv[1], sys.argv[2], self.port)
+			self.socket.datasocket = self.datasocket
 
-	def exit(self):
+
+	def pasvmode(self, response):
+		try:
+			pasv = response[26:].translate(None, "().\r\n").split(",")
+			p1 = int(pasv[-2])
+			p2 = int(pasv[-1])
+
+			port = (p1 * 256) + p2
+			self.port = port
+
+		except Exception as error:
+			self.ftplog.error(error)
+
+		self.datasocket = ClientSocket(sys.argv[1], sys.argv[2], self.port)
+		self.socket.datasocket = self.datasocket
+		self.command()
+
+	def exit(self, response):
 		self.ftplog.close()
 		exit(0)
 
@@ -176,25 +132,113 @@ class FTP:
 		return msg
 
 
-	def evaulate(self, response):
+	def buildport(self, cmd):
+		try:
+			host = self.socket.host
+			port = int(cmd[1])
 
+			remainderport = port % 256
+
+			#if remainder port greater than zero
+			p1 = (port - remainderport) / 256
+			p2 = remainderport
+
+			hostmsg = host.replace(".", ",")
+
+			self.port = port
+			msg = "%s %s,%s,%s\r\n" %(self.commands["port"], hostmsg, p1, p2)
+
+		except Exception as error:
+			self.ftplog.error(error)
+			self.ftplog.usage("port <port number>")
+			return None
+
+		return msg
+
+	def buildeprt(self, cmd):
+		try:
+			port = cmd[1]
+			host = self.socket.host
+			msg = "%s |%s|%s|%s|\r\n" %(self.commands["eprt"], 1, host, port)
+
+			return msg
+
+		except Exception as error:
+			self.ftplog.error(error)
+			self.ftplog.usage("eprt <port>")
+			return None
+
+	def buildretr(self, cmd):
+		try:
+			filepath = cmd[1]
+			msg = "%s %s\r\n" %(self.commands["retr"], filepath)
+			self.socket.send(msg)
+			self.socket.receive()
+			filedata = self.datasocket.receive()
+			with open(filepath, "w+") as openfile:
+				openfile.write(filedata)
+
+			self.socket.receive()
+			return None
+
+		except Exception as error:
+			self.ftplog.error(error)
+			self.ftplog.usage("retr <path to file>")
+			return None
+
+
+	def buildstor(self, cmd):
+		print "buildstor"
+
+		try:
+			filepath = cmd[1]
+			msg = "%s %s\r\n" %(self.commands["stor"], filepath)
+
+			# if not self.datasocket:
+			# 	self.openconnection()
+
+			content = ""
+
+			with open(filepath, 'r') as openfile:
+				content = openfile.read()
+				print content
+
+			self.socket.send(msg)
+			self.socket.receive()
+			self.datasocket.send(content)
+			self.datasocket.close()
+			self.socket.receive()
+
+		except Exception as error:
+			self.ftplog.error(error)
+			self.ftplog.usage("stor <path to file>")
+			return None
+
+
+
+	def evaulate(self, status, response):
+		print "Status: %s" %status
 		return {
-			"220" : self.promptusername, #220 FTP Server ready.
-			"214" : self.command, #214 The following commands are recognized:
-			"221" : self.exit, #221 Goodbye.
-			"226" : self.command, #226 Transfer complete.
-			"230" : self.command, #230 Login successful.
-			"228" : self.command, #228 Entering Passive Mode
-			"230" : self.command, #230 Login Succesfuluthorized
-			"250" : self.command, #250 "/" is the current directory.
-			"257" : self.command, #257 "/" is the current directory.
-			"331" : self.promptpassword, #331 Username ok, send password.
-			"332" : self.command, #332 Need account for login.
-			"421" : self.exit, #421 Control connection timed out.
-			"501" : self.command, #501 Syntax error in parameters or arguments.
-			"530" : self.command, #530 Authentication failed.
-			"550" : self.command, #550 No such file or directory.
-			}.get(response, self.command) #default just reprompt for command
+			"login" : (self.promptusername, response),
+			"125" : (self.openconnection, response),
+			"150" : (self.openconnection, response),
+			"220" : (self.promptusername, response),#220 FTP Server ready.
+			"214" : (self.command,response), #214 The following commands are recognized:
+			"221" : (self.exit, response),#221 Goodbye.
+			"227" : (self.pasvmode, response),
+			"226" : (self.command, response), #226 Transfer complete.
+			"230" : (self.command, response), #230 Login successful.
+			"228" : (self.command, response), #228 Entering Passive Mode
+			"230" : (self.command, response), #230 Login Succesfuluthorized
+			"250" : (self.command, response), #250 "/" is the current directory.
+			"257" : (self.command, response), #257 "/" is the current directory.
+			"331" : (self.promptpassword, response), #331 Username ok, send password.
+			"332" : (self.command, response), #332 Need account for login.
+			"421" : (self.exit, response), #421 Control connection timed out.
+			"501" : (self.command,response), #501 Syntax error in parameters or arguments.
+			"530" : (self.command, response), #530 Authentication failed.
+			"550" : (self.command, response), #550 No such file or directory.
+			}.get(status, (self.command, response)) #default just reprompt for command
 
 
 
@@ -204,6 +248,8 @@ class ClientSocket:
 		self.port = port
 		self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.log = logger(filename, "[client]")
+		self.clientsocket.settimeout(5)
+		self.datasocket = None
 
 		try:
 			self.clientsocket.connect((self.host, self.port))
@@ -212,7 +258,6 @@ class ClientSocket:
 
 		except socket.error as e:
 			self.log.error(str(e))
-			self.clientsocket = None
 			pass
 
 	def close(self):
@@ -220,15 +265,19 @@ class ClientSocket:
 
 
 	def receive(self):
+		response = ""
 		try:
-			message = self.clientsocket.recv(BUFFER)
-			self.log.received(message)
-		except struct.error as error:
-			self.log.error(error)
-			message = ""
-			pass
 
-		return message
+			message = self.clientsocket.recv(BUFFER)
+			response += message
+
+		except socket.error as error:
+			pass
+		if response:
+			self.log.received(response)
+		else:
+			print "did not recieve anything"
+		return response
 
 
 	def send(self, command):
@@ -239,6 +288,12 @@ class ClientSocket:
 		except struct.error as error:
 			self.log.error(error)
 			return False
+
+	def acceptconnection(self):
+		try:
+			self.clientsocket.accept()
+		except socket.timeout as error:
+			pass
 
 
 	def connected(self):
