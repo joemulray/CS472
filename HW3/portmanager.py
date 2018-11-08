37c56aefbacf0d89c6a965711d6c2768d70ef7c8
Joseph Mulray <joe.mulray@gmail.com>
2018-11-07 20:47:48 -0500

    update portmanager

--- HW3/portmanager.py
+++ HW3/portmanager.py
@@ -1,6 +1,10 @@
+import random
+
 class PortManager:
 	def __init__(self):
 		self.ports = []
+		self.lowerbound = 1024
+		self.upperbound = 65535
 
 	def exists(self, port):
 		return True if port in self.ports else False
@@ -8,3 +12,13 @@ class PortManager:
 	def addport(self, port):
 		self.ports.append(port)
 
+	def getport(self):
+		port = random.randint(self.lowerbound, self.upperbound)
+		while port in self.ports:
+			port = random.randint(self.lowerbound, self.upperbound)
+
+		self.ports.append(port)
+		return port
+
+	def removeport(self, port):
+		self.ports.remove(port)
